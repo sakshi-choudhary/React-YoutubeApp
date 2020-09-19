3d42ce7c71d5536e5f025526b8e0f796e9560978
Sakshi Choudhary <choudharysakshi023@gmail.com>
2020-09-19 14:06:35 +0530

    feat:semantic ui loader

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoDetail = ({ video }) => {
   if (!video) {
-    return <div>Loading...</div>;
+    return <div><div class="ui active centered inline loader"></div></div>;
   }
 
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
